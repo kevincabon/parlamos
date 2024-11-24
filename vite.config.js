0991e537e8c2d37bd800b5588aed80be031b6b0e
Kevin Cabon <45396141+kevincabon@users.noreply.github.com>
2024-11-24 11:35:36 +0100

    * feat(netlify.toml): add netlify.toml configuration file * feat(vite.config.js): add path module import and update alias for '@' to resolve to './src'

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   envDir: '.',
